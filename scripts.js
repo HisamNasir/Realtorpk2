8bb7e74e42df84effb7750c87ef6d52f1520d349
HisamNasir <hisamnasir1999@gmail.com>
2022-12-22 17:59:21 +0500

    update

--- scripts.js
+++ scripts.js
@@ -1,4 +1,9 @@
-
-// $(document).ready(function(){
-//   $(".mask.full, .circle .fill").css("transform: rotate(170deg);");
-// });
+var a = document.getElementById("city");
+var b = document.getElementById("property");
+var c = document.getElementById("price");
+var selectedcity = a.option[a.selectIndex].text;
+var selectedproperty = b.option[b.selectIndex].text;
+var selectedprice = c.option[c.selectIndex].text;
+function firstsectionsearchbtn(){
+    console.log(selectedcity);
+};
