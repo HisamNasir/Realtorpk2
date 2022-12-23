1e20f230fddebcf4349209351290c5f0abe5085c
HisamNasir <hisamnasir1999@gmail.com>
2022-12-23 16:06:18 +0500

    updated

--- scripts.js
+++ scripts.js
@@ -1,9 +1,18 @@
-var a = document.getElementById("city");
-var b = document.getElementById("property");
-var c = document.getElementById("price");
-var selectedcity = a.option[a.selectIndex].text;
-var selectedproperty = b.option[b.selectIndex].text;
-var selectedprice = c.option[c.selectIndex].text;
 function firstsectionsearchbtn(){
-    console.log(selectedcity);
-};
+    var a = document.getElementById("city");
+    var b = document.getElementById("property");
+    var c = document.getElementById("price");
+
+    var option1 = a.options[a.selectedIndex].text;
+    var option2 = b.options[b.selectedIndex].text;
+    var option3 = c.options[c.selectedIndex].text;
+
+    console.log("Your Selected City is: ",option1);
+    console.log("Your Selected Project Type is: ", option2);
+    console.log("Your Selected Price is: ", option3)
+
+
+}       
+    document.getElementById("demo1").innerHTML =  option1  ;
+    document.getElementById("demo2").innerHTML =  option2  ;
+    document.getElementById("demo3").innerHTML =  option3  ;
