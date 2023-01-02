cb37941752c1af1e92440fe460f502e7f61dabc3
HisamNasir <hisamnasir1999@gmail.com>
2023-01-02 15:27:19 +0500

    last update

--- scripts.js
+++ scripts.js
@@ -6,12 +6,5 @@ function firstsectionsearchbtn(){
     var option1 = a.options[a.selectedIndex].text;
     var option2 = b.options[b.selectedIndex].text;
     var option3 = c.options[c.selectedIndex].text;
-
-    console.log("Your Selected City is: ",option1);
-    console.log("Your Selected Project Type is: ", option2);
-    console.log("Your Selected Price is: ", option3);
-
-
-
 }       
    
