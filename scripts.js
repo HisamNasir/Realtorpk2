2c791de820417b2ac4a7b06000a70bf6ec3b8239
HisamNasir <hisamnasir1999@gmail.com>
2023-01-09 18:02:57 +0500

    update displaying specific location

--- scripts.js
+++
@@ -1,9 +0,0 @@
-function firstsectionsearchbtn(){
-    var a = document.getElementById("city");
-    var b = document.getElementById("property");
-    var c = document.getElementById("price");
-
-    var option1 = a.options[a.selectedIndex].text;
-    var option2 = b.options[b.selectedIndex].text;
-    var option3 = c.options[c.selectedIndex].text;
-}       
